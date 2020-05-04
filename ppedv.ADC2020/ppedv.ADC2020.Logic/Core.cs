71a7d06c49dd8b32b84efa4c66e0db0e15184fbf
André <ruland@outlook.de>
2020-05-04 11:58:58 +0200

    Consolen UI fertig

--- ppedv.ADC2020/ppedv.ADC2020.Logic/Core.cs
+++ ppedv.ADC2020/ppedv.ADC2020.Logic/Core.cs
@@ -1,10 +1,19 @@
 ﻿using ppedv.ADC2020.Data.EF;
+using ppedv.ADC2020.Model.Contracts;
 using System;
 
 namespace ppedv.ADC2020.Logic
 {
     public class Core
     {
+        public IRepository Repository { get; private set; }
 
+        public Core(IRepository repo)
+        {
+            Repository = repo;
+        }
+
+        public Core() : this(new Data.EF.EfRepository())
+        { }
     }
 }
---
+++ ppedv.ADC2020/ppedv.ADC2020.UI.DevConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using ppedv.ADC2020.Logic;
+using ppedv.ADC2020.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.ADC2020.UI.DevConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** ADC2020 DevConsole ***");
+
+            var core = new Core();
+
+            foreach (var a in core.Repository.GetAll<Auto>())
+            {
+                Console.WriteLine($"{a.Farbe}");
+            }
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
