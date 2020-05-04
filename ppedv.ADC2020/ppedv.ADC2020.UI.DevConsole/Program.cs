20643ac4d34ad8a7f11dada279050518ed985767
André <ruland@outlook.de>
2020-05-04 13:29:44 +0200

    IQueryable ist cool

--- ppedv.ADC2020/ppedv.ADC2020.UI.DevConsole/Program.cs
+++ ppedv.ADC2020/ppedv.ADC2020.UI.DevConsole/Program.cs
@@ -16,15 +16,15 @@ namespace ppedv.ADC2020.UI.DevConsole
 
             var core = new Core();
 
-            if (core.Repository.GetAll<Auto>().Count() == 0)
+            if (core.Repository.Query<Auto>().Count() == 0)
                 core.CreateDemoDaten();
 
-            foreach (var a in core.Repository.GetAll<Auto>())
+            foreach (var a in core.Repository.Query<Auto>().OrderBy(x => x.Created.Year).ThenBy(x => x.Farbe).ToList())
             {
                 Console.WriteLine($"{a.Hersteller},{a.Modell} {a.Farbe}");
                 foreach (var vm in a.Vermietungen)
                 {
-                    Console.WriteLine($"{vm.Start:D}-{vm.Ende:D} {vm.Kunde.Name}");
+                    Console.WriteLine($"\t{vm.Start:D}-{vm.Ende:D} {vm.Kunde.Name}");
                 }
             }
 
