4688fd6d9d35689b7b1f948b0cc6122faf6fa795
André <ruland@outlook.de>
2020-05-04 09:57:08 +0200

    Unittest beispiel

---
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Calc_Sum_3_and_4_results_9()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            int result = calc.Sum(4, 5);
+
+            //Assert
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_0_and_0_results_0()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            int result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_MAX_and_1_throws_OverflowException()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act + Assert
+            Assert.ThrowsException<OverflowException>(()=> calc.Sum(int.MaxValue, 1));
+
+        }
+    }
+}
---
+++ Calculator/Calculator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Calculator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5718457-a600-41fd-9973-15ac0927bfb5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
