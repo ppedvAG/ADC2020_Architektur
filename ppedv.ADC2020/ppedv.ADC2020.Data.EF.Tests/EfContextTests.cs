f381b9606c8823d64f1eb957f7297a329d75eeb7
André <ruland@outlook.de>
2020-05-04 11:21:35 +0200

    Data EF tests

--- ppedv.ADC2020/ppedv.ADC2020.Data.EF.Tests/EfContextTests.cs
+++ ppedv.ADC2020/ppedv.ADC2020.Data.EF.Tests/EfContextTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ppedv.ADC2020.Model;
 
 namespace ppedv.ADC2020.Data.EF.Tests
 {
@@ -17,6 +18,56 @@ namespace ppedv.ADC2020.Data.EF.Tests
             Assert.IsFalse(con.Database.Exists());
             con.Database.Create();
             Assert.IsTrue(con.Database.Exists());
+        }
+
+
+        [TestMethod]
+        public void EfContext_can_CRUD_Auto()
+        {
+            var auto = new Auto()
+            {
+                Farbe = $"Blau_{Guid.NewGuid().ToString().Substring(0, 12)}",
+                Hersteller = "Baudi",
+                Modell = "B99"
+            };
+
+            var neueFarbe = $"Rot_{Guid.NewGuid().ToString().Substring(0, 13)}";
+
+            using (var con = new EfContext())
+            {
+                con.Autos.Add(auto);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Autos.Find(auto.Id);
+
+                Assert.IsNotNull(loaded);
+                Assert.AreEqual(auto.Farbe, loaded.Farbe);
+
+                //UPDATE
+                loaded.Farbe = neueFarbe;
+                con.SaveChanges();
+            }
+
+            //check UPDATE
+            using (var con = new EfContext())
+            {
+                var loaded = con.Autos.Find(auto.Id);
+                Assert.AreEqual(neueFarbe, loaded.Farbe);
+
+                con.Autos.Remove(loaded);
+                con.SaveChanges();
+            }
+
+            //check DELETE
+            using (var con = new EfContext())
+            {
+                var loaded = con.Autos.Find(auto.Id);
+                Assert.IsNull(loaded);
+            }
+
         }
     }
 }
